7122196ff91a7f1cd221628d1619f2080a1c6424
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-11-06 19:45:55 -0500

    Ok it's cool now.

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -1127,9 +1127,9 @@ namespace lime {
 
 		int64_t nextTickTime = baseTime + (tickCounter + 1) * UPDATE_PERIOD;
 
-		// LAG DETECTION: If we're more than 2 frames behind, we lagged
+		// LAG DETECTION: If we're more than 2 render frames behind, we lagged
 		int64_t lagAmount = currentTime - nextTickTime;
-		if (lagAmount > UPDATE_PERIOD * 2) {
+		if (lagAmount > RENDER_PERIOD * 2) {
 			// Don't catch up - just reset the timeline
 			//printf("Lag detected: %lld us behind, resetting timeline\n", lagAmount);
 			baseTime = currentTime;
