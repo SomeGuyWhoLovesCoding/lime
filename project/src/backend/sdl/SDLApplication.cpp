79c4e61c5f6357c69568a7922e529fe323cb7b0b
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-03-04 16:23:28 -0500

    Revert "."
    This reverts commit a5cc287bc66e3055949d88ea42b224f6ea176ccf.

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -1102,7 +1102,6 @@ namespace lime
 		}
 	}
 
-	static int64_t lag = 0;
 	int64_t startTimestamp10ns = 0;
 
 	void SDLApplication::Init()
@@ -1176,6 +1175,7 @@ namespace lime
 		}
 	}
 
+	static int64_t lag = 0;
 	static int64_t minimalSleepCalc10ns = 0;
 
 	// Cached display mode — only refreshed when the window moves or on first call.
